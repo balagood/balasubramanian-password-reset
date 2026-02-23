7d23071e56e41bbf3ff1e61fa760ead3ae95478c
Balasubramanian G <balagomathinayagam@gmail.com>
2026-02-23 23:39:23 +0530

    update the mail file

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -6,16 +6,20 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   service: "SendGrid",
   //host: "smtp.gmail.com",
-  port: 465,
+  host: "smtp.sendgrid.net",
+  port: 587,
   secure: true, 
   auth: {
-    api_key: process.env.SENDGRID_API_KEY
+    //api_key: process.env.SENDGRID_API_KEY
     //user: process.env.EMAIL_USER,
     //pass: process.env.EMAIL_PASS
+    user: "apikey",
+    pass: process.env.SENDGRID_API_KEY
+
   },
   tls: {
-    family: 4 // force IPv4
-    //rejectUnauthorized: false
+    //family: 4 // force IPv4
+    rejectUnauthorized: false
   }
 
 });
