9df15690086753f9f8e2920282b54a360d2ed6b1
Balasubramanian G <balagomathinayagam@gmail.com>
2026-02-28 09:43:58 +0530

    adding th mail.js file

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -4,23 +4,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "SendGrid",
-  //host: "smtp.gmail.com",
-  host: "smtp.sendgrid.net",
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  //host: "smtp.sendgrid.net",
   port: 587,
-  secure: true, 
+  secure: false, 
   auth: {
     //api_key: process.env.SENDGRID_API_KEY
-    //user: process.env.EMAIL_USER,
-    //pass: process.env.EMAIL_PASS
-    user: "apikey",
-    pass: process.env.SENDGRID_API_KEY
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+   // user: "apikey",
+   // pass: process.env.SENDGRID_API_KEY
 
+  },tls: {
+    rejectUnauthorized: false, // ⚠️ disables certificate validation
   },
-  tls: {
-    //family: 4 // force IPv4
-    rejectUnauthorized: false
-  }
 
 });
 export default transporter;
